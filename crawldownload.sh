61fcc6d1fdd43f8a0c846c8548692296d4a0620c
池田 <numenshane@gmail.com>
2016-11-29 14:34:20 +0800

    Update crawldownload.sh

--- crawldownload.sh
+++ crawldownload.sh
@@ -4,6 +4,12 @@ if [ $# -lt 1 ]; then
     exit
 fi
 
+# steps
+# call scrapy to crawl web pages, pipeline metadata to files
+# processing files to abstract href links 
+# use download tools to pull data into local storage
+# mv local files to certain dir to sync to pcs
+
 keyword=$1
 scrapy crawl youtube -a search_flag="$1" -o "$1.json"
 sed -i.bak '/googleads/d' "$1.json"
