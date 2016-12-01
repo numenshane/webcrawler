8f6c1aca475336b0782e5ed94f4fee27de39eaa1
numenshane <numenshane@gmail.com>
2016-12-01 14:03:47 +0800

    ...

--- crawldownload.sh
+++ crawldownload.sh
@@ -15,6 +15,6 @@ scrapy crawl youtube -a search_flag="$1" -o "$1.json"
 sed -i.bak '/googleads/d' "$1.json"
 awk -F"\"" '{print $8}' $1.json >> $1.out
 for i in `cat $1.out`; do 
-    /usr/bin/youtubedownload "$i" 
+    /usr/bin/dl "$i" 
     echo "$i" >> "$1.completed"; 
 done
